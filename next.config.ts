c3bbc64f86ee0be611d2bc35fe6195b97e3dffcd
dungta.nkaitech <dungta.nkaitech@gmail.com>
2025-06-25 00:51:58 +0700

    Prepare for Netlify static deploy

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   images: {
-        domains: ['ichef.bbc.co.uk'], // Cho phép ảnh từ BBC
-    },
+    domains: ["ichef.bbc.co.uk"], // Cho phép ảnh từ BBC
+  },
 };
 
 export default nextConfig;
